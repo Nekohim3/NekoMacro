8e9c2e4663ed8722da60c3e8b794ae296d6ed39b
Nekohime <kuroneko@protonmail.ch>
2024-01-10 17:31:57 +0300

    10

---
+++ NekoMacro/MacrosBase/NewCmd/BaseCmd.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ReactiveUI;
+
+namespace NekoMacro.MacrosBase
+{
+    public enum CmdType
+    {
+        Key,
+        Mouse,
+        Repeat
+    }
+
+    public abstract class BaseCmd : ReactiveObject
+    {
+        private bool _ctrl;
+        public bool Ctrl
+        {
+            get => _ctrl;
+            set => this.RaiseAndSetIfChanged(ref _ctrl, value);
+        }
+
+        private bool _shift;
+        public bool Shift
+        {
+            get => _shift;
+            set => this.RaiseAndSetIfChanged(ref _shift, value);
+        }
+
+        private bool _alt;
+        public bool Alt
+        {
+            get => _alt;
+            set => this.RaiseAndSetIfChanged(ref _alt, value);
+        }
+
+        private int _delay;
+        public int Delay
+        {
+            get => _delay;
+            set => this.RaiseAndSetIfChanged(ref _delay, value);
+        }
+
+        private int _delayAfter;
+        public int DelayAfter
+        {
+            get => _delayAfter;
+            set => this.RaiseAndSetIfChanged(ref _delayAfter, value);
+        }
+
+        public abstract string Text { get; }
+
+        [JsonIgnore]
+        public abstract CmdType CmdType { get; }
+
+        public abstract void Execute();
+    }
+}
---
+++ NekoMacro/MacrosBase/NewCmd/KeyCmd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interceptor;
+using ReactiveUI;
+
+namespace NekoMacro.MacrosBase.NewCmd
+{
+    public class KeyCmd : BaseCmd
+    {
+        public override CmdType CmdType => CmdType.Key;
+
+        public override string Text => $"{(Ctrl ? "Ctrl+" : "")}{(Alt ? "Alt+" : "")}{(Shift ? "Shift+" : "")}{Key}";
+
+        private Keys _key;
+        public Keys Key
+        {
+            get => _key;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _key, value);
+                this.RaisePropertyChanged(Text);
+            }
+        }
+
+        public KeyCmd()
+        {
+            
+        }
+
+        public override void Execute()
+        {
+            
+        }
+    }
+}
